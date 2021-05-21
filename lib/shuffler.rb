dc4739f94f4c65f564d6e7dbf694c20de38e455b
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2021-05-21 11:26:49 -0600

    Add shuffle method on shuffle class

--- lib/shuffler.rb
+++ lib/shuffler.rb
@@ -4,4 +4,15 @@ class Shuffler
   def initialize(deck)
     @deck = deck
   end
+
+  def shuffle
+    shuffled_deck = []
+    until @deck.cards.empty? do
+      card = @deck.cards.sample
+      shuffled_deck << card
+      @deck.cards.delete(card)
+    end
+    @deck.cards = shuffled_deck
+    puts @deck.cards.map { |c| "#{c.rank} of #{c.suit}".capitalize }
+  end
 end
