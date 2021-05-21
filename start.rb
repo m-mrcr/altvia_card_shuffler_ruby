52374ca4d5cb2e82d24ed4edca86a6a7f2ceaf42
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2021-05-21 12:11:52 -0600

    Remove temporary code from start file

--- start.rb
+++ start.rb
@@ -1,12 +1,9 @@
-require 'pry'
 require './lib/shuffler'
 require './lib/deck'
 require './lib/card'
 
 ranks = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace']
 suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
-# ranks = ['2','3','4']
-# suits = ['hearts', 'spades']
 deck = Deck.new(ranks.flat_map { |rank| suits.map { |suit| Card.new(rank, suit) } })
 shuffler = Shuffler.new(deck)
 
