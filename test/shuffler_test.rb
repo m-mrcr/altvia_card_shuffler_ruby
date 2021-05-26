b65467df91a9679f8b5eea626200d1a91234d1c2
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2021-05-26 14:52:05 -0600

    Add extra cards

--- test/shuffler_test.rb
+++ test/shuffler_test.rb
@@ -9,6 +9,9 @@ class ShufflerTest < MiniTest::Test
     @ace_of_hearts = Card.new("ace", "hearts")
     @two_of_hearts = Card.new("two", "hearts")
     @three_of_hearts = Card.new("three", "hearts")
+    @four_of_hearts = Card.new("four", "hearts")
+    @five_of_hearts = Card.new("five", "hearts")
+    @six_of_hearts = Card.new("six", "hearts")
     @deck = Deck.new([@ace_of_hearts, @two_of_hearts, @three_of_hearts])
     @shuffler = Shuffler.new(@deck)
   end
@@ -22,6 +25,9 @@ class ShufflerTest < MiniTest::Test
   end
 
   def test_it_can_shuffle
+    # There is a slim chance the shuffled deck will
+    # return the original order due to the sample size.
+
     old_deck = @shuffler.deck.clone
     assert_equal old_deck.cards, @shuffler.deck.cards
     @shuffler.shuffle
